a31918d7dab79725e9d906c7b529d98d5beaee49
JASG123 <157982905+JASG123@users.noreply.github.com>
2024-03-19 19:03:29 +0800

    TicketPage

---
+++ helpdeskapp/app/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <p>
+        <Link href="/ticket">Ticket</Link>
+      </p>
+    </main>
+  );
+}
---
+++ helpdeskapp/app/ticket/page.js
@@ -0,0 +1,7 @@
+export default function TicketPage() {
+    return (
+        <main>
+            <h1>Here is Ticket Page</h1>
+        </main>
+    );
+}
